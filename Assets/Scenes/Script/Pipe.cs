6f42bf97e5cd77ac0bb33830e7750b7c22ab6fe2
ArielGoulart <31768080+ArielGoulart@users.noreply.github.com>
2020-11-24 14:53:09 -0300

    A
    A

---
+++ Assets/Scenes/Script/Pipe.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pipe : MonoBehaviour
+{
+
+    [Header("Sttings")]
+    [SerializeField] private float speed = 5f;
+
+
+    // Update is called once per frame
+    private void Update()
+    {
+        transform.Translate(new Vector3(-speed * Time.deltaTime, 0f, 0f), Space.World);
+    }
+}
